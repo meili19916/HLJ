ca2882ab9dca522cc561e789c790799862f096d5
HeLijuan <helijuan@HeLijuandeMacBook-Pro-2.local>
2017-05-31 11:06:26 +0800

    初始化项目

---
+++ HLJ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HLJ
+//
+//  Created by HeLijuan on 17/5/31.
+//  Copyright © 2017年 HeLijuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HLJ/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HLJ
+//
+//  Created by HeLijuan on 17/5/31.
+//  Copyright © 2017年 HeLijuan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
